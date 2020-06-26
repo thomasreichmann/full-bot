dbbfab4719a6d34a14ea95c75d5f7e56a45e3df0
Thomas Reichmann <thomasreichmann@hotmail.com>
2020-06-26 06:19:54 -0300

    roll command

---
+++ commands/roll.js
@@ -0,0 +1,9 @@
+const Discord = require('discord.js')
+const Functions = require(`../functions`)
+const fn = new Functions()
+
+exports.run = (/** @type {Discord.Client} */ client, /** @type {Discord.Message} */ message, args) => {
+    let max = fn.checkNull(args[0], 100)
+
+    message.reply(`${Math.floor(fn.random(max))}`)
+}

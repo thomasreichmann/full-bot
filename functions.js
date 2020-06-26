f079338fe9bd53420b52aa265f67cfb7576ad0e4
Thomas Reichmann <thomasreichmann@hotmail.com>
2020-06-26 06:18:17 -0300

    adapted functions to work with classes

--- functions.js
+++ functions.js
@@ -1,23 +1,13 @@
-if (typeof btoa === 'undefined') {
-    global.btoa = function (str) {
-        return new Buffer(str, 'binary').toString('base64');
-    };
-}
+module.exports = class Queue {
+    checkNull(element, value) {
+        element = element != undefined ? element : value
+        return element
+    }
 
-if (typeof atob === 'undefined') {
-    global.atob = function (b64Encoded) {
-        return new Buffer(b64Encoded, 'base64').toString('binary');
-    };
-}
-
-function checkNull(element, value) {
-    element = element != undefined ? element : value
-    return element
-}
-
-function random(min, max) { // Random com range entre numeros
-    if (min instanceof Array) return min[Math.round(Math.random() * (min.length - 1))];
-    min = checkNull(min, 0)
-    if (max === undefined && min > 0) return Math.random() * min
-    return Math.random() * (max - min) + min
+    random(min, max) { // Random com range entre numeros
+        if (min instanceof Array) return min[Math.round(Math.random() * (min.length - 1))];
+        min = this.checkNull(min, 0)
+        if (max === undefined && min > 0) return Math.random() * min
+        return Math.random() * (max - min) + min
+    }
 }

63dcf9ec9eb0c1bec08e686f6e520f71c741713c
Thomas Reichmann <thomasreichmann@hotmail.com>
2020-06-26 06:21:32 -0300

    imported functions

--- commands/ping.js
+++ commands/ping.js
@@ -1,7 +1,9 @@
 const Discord = require('discord.js')
+const Functions = require(`../functions`)
+const fn = new Functions()
 
 exports.run = ( /** @type {Discord.Client} */ client, /** @type {Discord.Message} */ message, args) => {
-    let delay = checkNull(args[0], 0)
+    let delay = fn.checkNull(args[0], 0)
 
     setTimeout(() => {
         message.reply(`Pong! ${(message.createdAt.valueOf() + (delay * 2)) - new Date().valueOf()}ms`)

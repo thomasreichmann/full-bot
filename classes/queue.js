1748e9a46e364b7e37131054071ff47f9b01e067
Thomas Reichmann <thomasreichmann@hotmail.com>
2020-07-14 00:17:38 -0300

    fixed several edge case bugs

--- classes/queue.js
+++ classes/queue.js
@@ -23,10 +23,10 @@ module.exports = class Queue {
     end() {
         console.log(`Finalizando queue no servidor: "${this.guild.name}" | "${this.guild.id}"`)
         this.playing = false;
-        this.songs = undefined
-        this.dispatcher.destroy()
+        this.songs = undefined;
+        if (this.dispatcher) this.dispatcher.destroy();
         this.connection.disconnect();
-        this.client.queues[this.guild.id] = undefined
+        this.client.queues[this.guild.id] = undefined;
     }
 
     skip() {
@@ -41,18 +41,27 @@ module.exports = class Queue {
     }
 
     async play() {
-        this.dispatcher = this.connection.play(await ytdl(this.songs[0].url), {
-            type: 'opus'
-        })
+        try {
+            this.dispatcher = this.connection.play(await ytdl(this.songs[0].url, {
+                filter: 'audioonly',
+                highWaterMark: 1 << 25
+            }), {
+                type: 'opus'
+            })
+        } catch (err) {
+            console.error(err)
+        }
 
         this.playing = true
 
-        this.dispatcher.on(`finish`, () => {
+        this.dispatcher.on(`finish`, (reason) => {
+            console.log(`Reason: ${reason}`)
+
             let video = this.songs.shift()
 
             // Console.log longo para debug
             console.log(`Video removido: "${video.title}"\n URL: "${video.url}"\n Guild: "${this.guild.name}" ID: "${this.guild.id}"\n`)
-            if(this.songs.length > 0) {
+            if (this.songs.length > 0) {
                 this.play()
             } else {
                 this.playing = false;
@@ -61,7 +70,7 @@ module.exports = class Queue {
         })
 
         this.dispatcher.on(`error`, (err) => {
-            console.log(err)
+            console.log(`Erro dispatcher error event:\n${err}`)
         })
     }
 }

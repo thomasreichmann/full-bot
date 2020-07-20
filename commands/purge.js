4b543045542f105758144bce6c44e6bbcaba8f45
Thomas Reichmann <thomasreichmann@hotmail.com>
2020-07-20 11:17:08 -0300

    added permission req

--- commands/purge.js
+++ commands/purge.js
@@ -4,6 +4,7 @@ module.exports = {
 	name: 'purge',
 	description: 'Deleta um numero de mensagens de um canal.',
 	usage: 'purge [n]',
+	permissions: 8192,
 	async execute(/** @type {Discord.Client} */ client, /** @type {Discord.Message} */ message, args) {
 		let amt = typeof args[0] === 'string' ? args[0] : 10;
 		amt = parseInt(amt) + 1;
@@ -12,7 +13,7 @@ module.exports = {
 		let messages = await message.channel.bulkDelete(amt, true);
 		let m = await message.channel.send({
 			'embed': {
-				'color': 7536755,
+				'color': client.config.color,
 				'fields': [{
 					'name': 'Mensagens deletadas :negative_squared_cross_mark:',
 					'value': `${messages.size - 1} ${amt - 1 === 1 ? 'mensagem removida' : 'mensagens removidas'}`,
